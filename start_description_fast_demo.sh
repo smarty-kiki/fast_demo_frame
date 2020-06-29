2ed90a54d76c90d63bc8e2a15458c8304e43a64f
yaoyang <kiki@smarty.so>
2020-06-29 23:34:09 +0800

    更新 fast_demo_frame 脚本

--- start_description_fast_demo.sh
+++ start_description_fast_demo.sh
@@ -4,4 +4,7 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/
 
 sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name description_fast_demo \
     -v $ROOT_DIR/description:/var/www/mvc_frame/domain/description \
+    -e 'ENV=development' \
+    -e 'TIMEZONE=Asia/Shanghai' \
+    -e 'AFTER_START_SHELL=/var/www/mvc_frame/project/tool/development/fast_demo_watch.sh' \
 kikiyao/description_fast_demo start
